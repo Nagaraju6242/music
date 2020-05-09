81c32cbd0e1ee2e5f6c93b975a40f4c65b440930
Nagaraju6242 <62198428+Nagaraju6242@users.noreply.github.com>
2020-05-09 16:08:26 +0530

    Add files via upload

---
+++ mouse.js
@@ -0,0 +1,46 @@
+var s = 0;
+var k = 0;
+var a = new Audio( 'songs/' + s + '.mp3' );
+
+function min(x){
+    document.body.style.background = "#e74c3c";
+    x.className = 'btn';
+    a.play();
+}
+
+function mout(x){
+    if(k == 0){
+        document.body.style.background = "#95a5a6" ;
+        x.className = 'stop';
+        a.pause();
+    }
+}
+
+function mc(){
+    var x = document.getElementById('but');
+    if(k == 0){
+        document.body.style.background = "#e74c3c";
+        x.className = 'btn';
+        a.play();
+        k = 1;
+    }
+    else if(k == 1){
+        document.body.style.background = "#95a5a6" ;
+        x.className = 'stop';
+        a.pause();
+        k = 0;
+    }
+}
+
+function change(){
+    var x = document.getElementById('but');
+    a.pause();
+    s++;
+    if(s == 11)
+        s = 0;
+    a = new Audio( 'songs/' + s + '.mp3' );
+    document.body.style.background = "#e74c3c";
+    x.className = 'btn';
+    a.play();
+    k = 1;
+}
