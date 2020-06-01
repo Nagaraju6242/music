643a39d24b893fe5dd2b57cdaa6ad8140eff5413
Nagaraju6242 <62198428+Nagaraju6242@users.noreply.github.com>
2020-06-01 18:32:42 +0530

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -1,6 +1,12 @@
-var s = -2;
+//jshint esversion:6
+
+var s = 0;
 var k = 0;
-var a = new Audio( 'songs/' + s + '.mp3' );
+var a = document.getElementById("but");
+var x = document.getElementById('button');
+var songs = Array.from(Array(12).keys());
+songs.sort(() => Math.random() - 0.5);
+a.src = "songs/" + songs[s] + ".mp3";
 
 function min(x){
     document.body.style.background = "#e74c3c";
@@ -8,37 +14,41 @@ function min(x){
     a.play();
 }
 
-function mout(x){
+function mout(y){
     if(k == 0){
         document.body.style.background = "#95a5a6" ;
-        x.className = 'stop';
+        y.className = 'stop';
         a.pause();
     }
 }
 
-function mc(){
-    var x = document.getElementById('but');
+function mc(y){
     if(k == 0){
         document.body.style.background = "#e74c3c";
-        x.className = 'btn';
+        y.className = 'btn';
         a.play();
         k = 1;
     }
     else if(k == 1){
         document.body.style.background = "#95a5a6" ;
-        x.className = 'stop';
+        y.className = 'stop';
         a.pause();
         k = 0;
     }
 }
 
+setInterval(function(){
+  if(a.ended == true){
+    change();
+  }
+} , 7000);
+
 function change(){
-    var x = document.getElementById('but');
     a.pause();
     s++;
-    if(s == 11)
-        s = -2;
-    a = new Audio( 'songs/' + s + '.mp3' );
+    if(s == 12)
+        s = 0;
+    a.src = "songs/" + songs[s] + ".mp3";
     document.body.style.background = "#e74c3c";
     x.className = 'btn';
     a.play();
